a8d15857db29eb8d18f11484b06e3cc4d4f78f29
Jonathan Contreras <jonathancontreras@Jonathans-MacBook-Air.local>
2023-07-12 16:32:55 -0500

    some logic 1 problems solved

--- Logic-1/squirrel_play.py
+++ Logic-1/squirrel_play.py
@@ -9,6 +9,12 @@
 # squirrel_play(95, True) → True
  
 def squirrel_play(temp, is_summer):
-    pass
+    if is_summer:
+        if temp >= 60 and temp <= 100: return True
+        else: return False
+    elif not is_summer:
+        if temp >= 60 and temp <= 90: return True
+        else: return False
 
-print(squirrel_play(70, False))
+
+print(squirrel_play(95, True))
